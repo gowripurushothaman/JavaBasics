bd7aaa809c553dcced019a19e99c5a243a7f1fe3
gowripurushothaman <gowrikalpana27@gmail.com>
2018-05-29 16:05:07 +0530

    String program

---
+++ src/com/basic/Test.java
@@ -0,0 +1,9 @@
+package com.basic;
+
+public class Test {
+	public static void main(String[] args) {
+		String firstName="Gowri";
+		String lastName="Purushothaman";
+		System.out.println(firstName+" "+lastName);
+	}
+}
