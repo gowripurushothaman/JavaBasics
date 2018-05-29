c5ccd48211c05fa97320159082104dd95ae0701d
gowripurushothaman <gowrikalpana27@gmail.com>
2018-05-29 18:17:53 +0530

    WhileTask3

---
+++ src/com/basic/WhileTask2.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class WhileTask2 {
+	public static void main(String[] args) {
+		int x=50,total=0;
+		while ( x <= 100 ) {
+			total += x;
+			++x;
+			
+		}
+		System.out.println("total="+total);
+			
+
+
+	}
+
+}
