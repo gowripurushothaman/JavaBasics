33f370473d8042f146980c5446ac18a5eb50969d
gowripurushothaman <gowrikalpana27@gmail.com>
2018-05-29 18:11:08 +0530

    TaskCode

---
+++ src/com/basic/TaskCode.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class TaskCode {
+	public static void main(String[] args) {
+	int x = 1, total=1;
+	while ( x <= 10 )
+	{
+	total += x;
+	++x;
+	}
+	System.out.println("total ="+ total);
+
+	}
+
+}
