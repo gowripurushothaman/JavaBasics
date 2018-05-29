8ff27d6c1afc39977badab826e16876e9e905ea4
gowripurushothaman <gowrikalpana27@gmail.com>
2018-05-29 16:10:25 +0530

    DataTypeNumber

---
+++ src/com/basic/DataTypeNumber.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeNumber {
+	public static void main(String[] args) {
+		String firstName="Gowri";
+		String lastName="Purushothaman";
+		System.out.println(firstName+" "+lastName);
+	}
+
+}
