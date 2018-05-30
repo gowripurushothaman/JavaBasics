a0452722d04c5e7748a8c4462fd8bbacd9eee75b
gowripurushothaman <gowrikalpana27@gmail.com>
2018-05-30 16:33:29 +0530

    ClassExample

---
+++ src/com/basic/Car.java
@@ -0,0 +1,42 @@
+package com.basic;
+
+public class Car {
+		double speed;
+		String model;
+		public Car() {
+			
+		}
+		
+		public double getSpeed() {
+			return speed;
+		}
+
+		public void setSpeed(double speed) {
+			this.speed = speed;
+		}
+
+		public String getModel() {
+			return model;
+		}
+
+		public void setModel(String model) {
+			this.model = model;
+		}
+
+		public Boolean Start() {
+			return true;
+			
+		}
+		public void Accelarate() {
+			speed+=10;
+		}
+		public static void main(String[] args) {
+		Car car1=new Car();
+		Car car2=new Car();
+		car1.speed=20.1;
+		car2.speed=30.0;
+		System.out.println(car1.speed);
+		System.out.println(car2.speed);
+		
+	    }
+}
