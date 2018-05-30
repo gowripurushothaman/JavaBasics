77d9d01c1c744266ae7a58c49c113118da8a81c6
gowripurushothaman <gowrikalpana27@gmail.com>
2018-05-30 09:42:51 +0530

    TypeCasting

---
+++ src/com/basic/TypeCasting.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class TypeCasting {
+ public static void main(String[] args) {
+	 byte firstNumber=10;
+	 byte secondNumber=110;
+	 byte total=(byte)(firstNumber+secondNumber);
+	 System.out.println("total="+total);
+ }
+}
