fd705e32e9bb6fd7ef3048d5b6aa3b700d866aea
gowripurushothaman <gowrikalpana27@gmail.com>
2018-05-30 10:48:50 +0530

    Switchcase

---
+++ src/com/basic/Switchcase.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class Switchcase {
+	public static void main(String[] args) {
+		int a=10;
+		switch(a) {
+		case 5:System.out.println("Given value is valuable");break;
+		case 10:System.out.println("Given value is valuable");break;
+		case 15:System.out.println("Given value is valuable");break;
+		case 20:System.out.println("Given value is valuable");break;
+		default:System.out.println("Invalid variable");
+		}
+		
+	}
+
+}
