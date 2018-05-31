b2290c4a9dea250db1b1e8ffc1865e2988a27070
gowripurushothaman <gowrikalpana27@gmail.com>
2018-05-31 15:24:42 +0530

    Multiple and even

---
+++ src/com/basic/Arithmatic.java
@@ -0,0 +1,48 @@
+package com.basic;
+class Multiply{
+	int firstValue;
+	int secondValue;
+	public Multiply(int firstValue,int secondValue) {
+		this.firstValue=firstValue;
+		this.secondValue=secondValue;
+		
+	}
+	public int getFirstValue() {
+		return firstValue;
+	}
+	public void setFirstValue(int firstValue) {
+		this.firstValue = firstValue;
+	}
+	public int getSecondValue() {
+		return secondValue;
+	}
+	public void setSecondValue(int secondValue) {
+		this.secondValue = secondValue;
+	}
+	public Boolean multiple(int firstValue,int secondValue) {
+		if(secondValue==(firstValue*firstValue)) {
+			return true;
+		}
+		else
+			return false;
+	}
+	public void even(int firstValue) {
+		if(firstValue%2==0)
+			System.out.println("The given number is Even");
+		else
+			System.out.println("The given number is not Even");
+	}
+	
+}
+
+public class Arithmatic {
+	public static void main(String[] args) {
+		Multiply m=new Multiply(3,9);
+		m.multiple(3,9);
+		m.setFirstValue(12);
+		m.even(m.getFirstValue());
+		
+		
+	}
+
+}
