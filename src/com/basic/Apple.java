8044de5e1270d33cdea3256ba20b092c7aa733c6
gowripurushothaman <gowrikalpana27@gmail.com>
2018-06-04 14:23:51 +0530

    Abstract

---
+++ src/com/basic/Apple.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class Apple extends Fruits {
+
+	@Override
+	public void fruits() {
+		// TODO Auto-generated method stub
+		System.out.println("apple");
+	}
+	
+	
+
+}
---
+++ src/com/basic/Fruits.java
@@ -0,0 +1,9 @@
+package com.basic;
+
+public abstract class Fruits {
+	public abstract void fruits();
+public static void main(String[] args) {
+	Apple apple=new Apple();
+	apple.fruits();
+}
+}
