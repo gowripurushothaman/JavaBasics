6fa75435de468bf2cc14338953163d1b7a62b1a0
gowripurushothaman <gowrikalpana27@gmail.com>
2018-06-06 10:50:35 +0530

    Printing.java

---
+++ src/com/basic/Printing.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class Printing {
+	public static void main(String[] args) {
+		int a[][]=new int[10][5];
+		for ( int i = 1; i <= 10; i++ )
+		{
+		for ( int j = 1; j <= 5; j++ ) {
+		System.out.print( '@' );
+		}System.out.println();
+		}
+		 
+		  // end outer for
+		 } // end main
+	// end class Printing
+
+	}
+
