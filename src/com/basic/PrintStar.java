2a0dc39ac6a2378a816b1fc3bf6a9e124d9e024b
gowripurushothaman <gowrikalpana27@gmail.com>
2018-06-06 10:51:53 +0530

    print

---
+++ src/com/basic/PrintStar.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class PrintStar {
+	public static void main(String[] args) {
+		int a[][][]=new int[5][3][4];
+		int i,j,k;
+		for (i= 1; i <= 5; i++ )
+		{
+		for (j= 1; j <= 3; j++ )
+		{
+		for (k= 1; k <= 4; k++ )
+		System.out.print( '*' );
+		System.out.println();
+		} // end inner for
+		System.out.println();
+		} // end outer for
+
+	}
+
+}
