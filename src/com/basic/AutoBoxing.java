8c7042c7980e38530a2077232bd039c4ee77f9ac
gowripurushothaman <gowrikalpana27@gmail.com>
2018-06-14 11:44:55 +0530

    AutoBoxing example program

---
+++ src/com/basic/AutoBoxing.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class AutoBoxing {
+	public static void main(String args[]) {
+		Integer val=new Integer(10);
+		val++;
+		int k=10+val;
+		System.out.println(val);
+		System.out.println(k);
+	}
+}
