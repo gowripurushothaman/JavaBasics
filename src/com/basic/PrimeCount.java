6cfaf1fb86b4aad14ee30b95a97645dc73f58183
gowripurushothaman <gowrikalpana27@gmail.com>
2018-06-25 14:32:13 +0530

    Prime Number Count

--- src/com/basic/PrimeCount.java
+++ src/com/basic/PrimeCount.java
@@ -3,9 +3,9 @@ package com.basic;
 public class PrimeCount {
 	public static void main(String args[]) {
 		int input1, input2 = 100, count = 0, count1 = 0;
-		for (input1 = 2; input1 <= input2; input1++) {
+		for (input1 = 1; input1 <= input2; input1++) {
 			//most of the prime number is odd number
-			if (input1 % 2 != 0) {
+			if ((input1 % 2 != 0)||(input1%2==0)) {
 				for (int i = 1; i <= input1; i++) {
 					if (input1 % i == 0) {
 						count++;
